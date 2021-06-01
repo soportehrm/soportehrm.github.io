e79ce3f4ad9a16ff18cc4331c49942a1e3936853
Ernesto Lozano Gonzalez <soporte@realdeminas.com>
2021-06-01 14:30:51 -0500

    t2

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <iframe src="https://survey.reviewpro.com/feedback/survey/YLAoObfhWk3eCaMR" width="100%" height="100%"></iframe>
+</body>
+
+</html>
+
